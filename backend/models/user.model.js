b732155dd205df32c9f8e5e3db461eed1857dbb8
sadxdev <sadikxdev@gmail.com>
2026-01-22 17:10:28 +0530

    login, logout, signup and refresh token completed

--- backend/models/user.model.js
+++ backend/models/user.model.js
@@ -42,23 +42,17 @@ const userSchema = new mongoose.Schema(
   }
 );
 
-const User = mongoose.model('User', userSchema);
+userSchema.pre('save', async function () {
+  if (!this.isModified('password')) return;
 
-userSchema.pre('save', async function (next) {
-  if (!this.isModified('password')) return next();
-
-  try {
-    const salt = await bcrypt.genSalt(10);
-    this.password = await bcrypt.hash(this.password, salt);
-    next();
-  } catch (error) {
-    next(error);
-  }
+  const salt = await bcrypt.genSalt(10);
+  this.password = await bcrypt.hash(this.password, salt);
 });
 
 userSchema.methods.comparePassword = async function (password) {
   return bcrypt.compare(password, this.password);
 };
 
+const User = mongoose.models.User || mongoose.model('User', userSchema);
 
 export default User;

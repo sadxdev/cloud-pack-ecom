de80a6672e6ae182737458b31e2e04b27b4fc63d
sadxdev <sadikxdev@gmail.com>
2026-01-28 09:46:48 +0530

    server changes

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import cookieParser from 'cookie-parser';
+import path from 'path';
 
 import authRoutes from './routes/auth.route.js';
 import productRoutes from './routes/product.route.js';
@@ -10,12 +11,15 @@ import paymentRoutes from './routes/payment.route.js';
 import analyticsRoutes from './routes/analytics.route.js';
 
 import { connectDB } from './lib/db.js';
+
 dotenv.config();
 
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5001;
 
-app.use(express.json({ limit: '10mb' }));
+const __dirname = path.resolve();
+
+app.use(express.json({ limit: '10mb' })); // allows you to parse the body of the request
 app.use(cookieParser());
 
 app.use('/api/auth', authRoutes);
@@ -25,8 +29,15 @@ app.use('/api/coupons', couponRoutes);
 app.use('/api/payments', paymentRoutes);
 app.use('/api/analytics', analyticsRoutes);
 
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '/frontend/dist')));
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'frontend', 'dist', 'index.html'));
+  });
+}
+
 app.listen(PORT, () => {
   console.log('Server is running on http://localhost:' + PORT);
-
   connectDB();
 });
